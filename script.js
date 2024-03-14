91d34e73cad354aca114ef1399810c76eb9211d0
Rocky Mor <rockymor2003@gmail.com>
2024-03-14 17:13:25 +0530

    done some revision of Object.create classes based revision and some things  about constructor function and how to link there prototype chain

--- script.js
+++ script.js
@@ -24,3 +24,19 @@ Dog.prototype = Person.prototype;
 const Dogs = new Dog("Rocky", "Tot");
 const value2 = Dogs.greet();
 console.log(value2);
+
+const value44 = {
+  name: "Rocky",
+  method() {
+    console.log(this.name);
+  },
+};
+
+const value33 = Object.create(value44);
+
+value33.method2 = function () {
+  console.log(this.name);
+};
+
+value33.method();
+value33.method2();
